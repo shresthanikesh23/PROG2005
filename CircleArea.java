3d646ac54b76d4d5fd11c4d51bffb07ac83ff757
shresthanikesh23 <shresthanikesh23@gmail.com>
2023-08-28 10:23:42 +1000

    This is the updated CircleArea file.

--- CircleArea.java
+++ CircleArea.java
@@ -5,10 +5,11 @@
  * @author (Nikesh)
  * @version (28/08/2023)
  */
+import java.util.Scanner;
 public class CircleArea
 {
     // instance variables
-    private double radius = 2.5;
+    private double radius;
     private double area;
 
     /**
@@ -20,13 +21,15 @@ public class CircleArea
     }
     
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double radius = input.nextDouble();
         CircleArea circle1 = new CircleArea();
-        circle1.getArea();
+        circle1.getArea(radius);
     }
 
-    public void getArea()
+    public void getArea(double radius)
     {
-        this.area = this.radius * this.radius * 3.14;
+        area = radius * radius * 3.14;
         System.out.println("The Circle Area = "+this.area);
     }
 }
